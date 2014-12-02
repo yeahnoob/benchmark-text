e56cc247e10be923c7fb72b6c3cb5f22bd331427
Xanadu Zhang <zgemlbox@gmail.com>
2014-12-02 23:40:56 +0800

    using Hash-Index algorithm, which is more fit for Julialang, 200% faster

--- julia/julia-perf/countpairs-opt.jl
+++ julia/julia-perf/countpairs-opt.jl
@@ -1,46 +1,55 @@
 ### Commentary:
 ### With JuliaLang v0.3.3
+## A Hash-Index algorithm for the text file's word-pairs counting. 
+## It's seemed that Julialang's Dict data structure with String keywords 
+## is slower than it with Integer keywords.
 
-function processdata!(filename::String, idx::Dict{String, Array})
+# English words' Hash Function borrowed from http://www.cse.yorku.ca/~oz/hash.html
+function hashdjb2(t::String)
+    ### used for english words' hash
+
+    ## ha = convert(Uint,5381)
+    ha = 0x0000000000001505
+    for c in t
+        ha = (ha<<5 + ha)>>3 + int(c)
+    end
+    ha
+end
+
+function processdata!( filename::String, idx::Dict{Uint, Array{Uint}}; myhash::Function = hashdjb2 )
     ### result store in index variable "idx"
     
     ## sort lines
-    lines = sort(readlines(open(filename, "r")))
-    
-    ## fill the index
-    firstone = 1
-    lastone = 0
-    prekey = ""
-    for item in lines
-        key = split(strip(item), ',', 2)
-        if key[1] == prekey
-            lastone += 1
+    W = String[]
+    W = readdlm(filename, ' ', String)
+    const linescount = size(W,1)
+
+    ## get a index, [ HashValueOfKeyword ---> [MatchedLinesNumber, ...] ]
+    for i in 1:linescount
+        key = myhash(W[i,1])
+        if haskey(idx,key)
+            push!(idx[key], i)
         else
-            idx[prekey] = [firstone lastone]
-            lastone += 1
-            firstone = lastone
-            prekey = key[1]
+            idx[key] = [i]
         end
     end
-    return idx
 end
 
-function bench(loops::Int64, filename::String, keyword::String)
-    global_idx = Dict{String, Array}()
-    #println("... Processing ", filename, " ", loops, " Times ...")
+function bench( loops::Int64, filename::String, keyword::String; mybenchhash::Function = hashdjb2 )
+    ### benchmark processdata() function
+
+    println("\n... Process [", filename, "] and count pairs with [", keyword, "] for [", loops, "] Times ...")
+    println("... hash words with function [", string(mybenchhash), "]")
     for i = 1:loops
-        #= wait for sevaral seconds, take easy.:) Hardisk I/O
-        y = 0
-        for i = 1:10^8
-            ret = i
-            ret = i + 2
-            ret = i + 1
-            y += ret
-        end
-        =#
-        processdata!(filename, global_idx)
-        println( "...... pairs of [\"", keyword, "\"] = ", global_idx[keyword]  )
+        global_idx = Dict{Uint, Array{Uint}}()
+        sizehint(global_idx, typemax(Uint16))
+        @time processdata!(filename, global_idx; myhash = mybenchhash)
+        println( "...... pairs of [", keyword, "] = ", length(global_idx[mybenchhash(keyword)]) )
     end
 end
 
-None 
+bench( int(ARGS[1]), "wordpairs.txt", string(ARGS[2]) )
+## Around 10% slower if using Julia's default hash() function
+#bench( int(ARGS[1]), "wordpairs.txt", string(ARGS[2]); mybenchhash = hash )
+
+None
