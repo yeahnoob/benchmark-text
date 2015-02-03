8c46f5d99706ed3b4b17ee8e796f28e0d0b862c0
yeahnoob <zgemlbox@gmail.com>
2015-02-03 16:42:58 +0800

    add rust-lang implementation of wordpairs bench

---
+++ rust/wordpairs/src/main.rs
@@ -0,0 +1,51 @@
+#![feature(path)]
+#![feature(io)]
+#![feature(os)]
+#![feature(core)]
+
+use std::old_io::BufferedReader;
+use std::old_io::File;
+use std::collections::HashMap;
+use std::os;
+
+fn main() {
+    let arguments: Vec<String> = os::args(); 
+    println!("reading the file: \"{}\"", arguments[1]);
+    let path = Path::new(&*arguments[1]);
+    let mut file = BufferedReader::new(File::open(&path));
+    let alllines: Vec<String> = file.lines().map(|x| x.unwrap()).collect();
+
+    // if debug {
+    // let mut last_firstword: String = "".to_string();
+    // let mut last_lastword: String = "".to_string();
+
+    // store the file into a HashMap of Vector `textpairs`.
+    let mut textpairs: HashMap<&str, Vec<&str>> = HashMap::new();
+    for line in alllines.iter() {
+        let twowords: Vec<&str> = line.words().collect();
+
+        /*
+        if twowords.len() < 2 {
+            //if debug {
+            // println!("the last line two words: {}...{}", last_firstword, last_lastword);
+            println!("the panic word: {:?}", twowords);
+            panic!("The input file ERROR.\n One panic line at least, which only contains One Word!!!");
+        }
+        */
+
+        let firstword = twowords[0];
+        let lastword = twowords[1];
+        
+        // if debug {
+        // last_firstword = firstword.to_string();
+        // last_lastword = lastword.to_string();
+
+        if !textpairs.contains_key(&firstword) {
+            textpairs.insert(&firstword, Vec::new());
+        } else {
+            textpairs.get_mut(&firstword).unwrap().push(&lastword);
+        }
+    }
+
+    println!("{:?}", textpairs.get(&*arguments[2]).unwrap().len());
+}
